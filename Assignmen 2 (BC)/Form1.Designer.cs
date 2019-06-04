2f2c75b286267abdc35d3248a1756f54f1b511ff
rc0032716 <rc0032716@CRO-24-01>
2019-06-04 10:41:25 +0100

    started to add code

--- Assignmen 2 (BC)/Form1.Designer.cs
+++ Assignmen 2 (BC)/Form1.Designer.cs
@@ -329,6 +329,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
--- Assignmen 2 (BC)/Form1.cs
+++ Assignmen 2 (BC)/Form1.cs
@@ -16,5 +16,16 @@ namespace Assignmen_2__BC_
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Calculate the total resistance in the series circuit of 2 resistors
+            try
+            {
+                Double R1, R2, RT;
+                R1 = Convert.ToDouble(textBox1.Text);
+                R2 = Convert.ToDouble(textBox2.Text);
+            }
+        }
     }
 }
